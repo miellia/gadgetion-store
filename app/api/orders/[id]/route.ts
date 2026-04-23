5bbbe04240cd0e9476f9243050c2cdb3d874600f
miellia <miellia@users.noreply.github.com>
2026-04-24 04:45:49 +0500

    changes in routes

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -3,9 +3,10 @@ import prisma from "@/lib/prisma";
 
 export async function PUT(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
+    const { id } = await params;
     const body = await req.json();
     const { status } = body;
 
@@ -17,7 +18,7 @@ export async function PUT(
     }
 
     const order = await prisma.order.update({
-      where: { id: params.id },
+      where: { id },
       data: { status },
     });
 
